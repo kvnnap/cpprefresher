efe9486bf11e53127c007aa08a0eb4a8c128c9f4
Kevin Napoli <kvnnap@gmail.com>
2026-02-05 10:22:01 +0100

    Learning...

--- main.cpp
+++ main.cpp
@@ -1,17 +1,9 @@
 import std;
-
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
-
 import test;
 
-int main(int argc, const char** argv)
+int main(int, const char**)
 {
-    vector<int> a;
-    a.push_back(1);
-    cout << "hello: " << string(argv[0]) << ":" << a.at(0) << endl;
-    cout << "add: " << subtract(1, 2) << endl;
+    using namespace std;
+    println("Println yay: {}", add(2,3));
     return 0;
 }
